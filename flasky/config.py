180d198afb636abd72f859329aa84b8d7fa76ec3
garaba.vlad <garaba.vlad@gmail.com>
2020-08-25 12:12:16 +0300

    added config.py for deployment

---
+++ flasky/config.py
@@ -0,0 +1,26 @@
+from flasky import app
+
+# admin accounts
+app.config['ADMIN_LIST'] = ['garaba1u', 'garaba.vlad@gmail.com']
+
+# setting app secret
+app.secret_key = 'dasWF@#56$VP"as1'
+
+# DATABASE MySQL config
+app.config['MYSQL_HOST'] = 'j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+app.config['MYSQL_USER'] = 'apgcuzyplwnhtpzf'
+app.config['MYSQL_PASSWORD'] = 'wibpgq8uv06eciat'
+app.config['MYSQL_DB'] = 'fwtyv7la1cjn796i'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+# Configuring Flask-Mail
+app.config['MAIL_SERVER']  = 'smtp.gmail.com'
+app.config['MAIL_PORT']  = 465
+app.config['MAIL_USE_SSL']  = True
+app.config['MAIL_DEBUG']  = app.debug
+app.config['MAIL_USERNAME']  = 'garaba.vlad1@gmail.com'
+app.config['MAIL_PASSWORD']  = 'leopartino0'
+app.config['MAIL_DEFAULT_SENDER']  = 'no-reply@flasky-article-app.herokuapp.com'
+app.config['MAIL_MAX_EMAILS']  = None
+app.config['MAIL_SUPPRESS_SEND']  = app.testing
+app.config['MAIL_ASCII_ATTACHMENTS']  = False
