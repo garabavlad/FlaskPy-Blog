2c8a9e314cae4c33a0911cd4282344ba341fd8e4
garaba.vlad <garaba.vlad@gmail.com>
2020-08-25 18:33:17 +0300

    Fixed flash message after activating

--- flasky/views.py
+++ flasky/views.py
@@ -178,10 +178,7 @@ def activate():
 
         if (data['username'] in app.config['ADMIN_LIST'] or data['email'] in app.config['ADMIN_LIST']):
             session['admin'] = True
-            if data['activated']:
-                flash("Your account is already activated!", "warning")
-            else:
-                flash("Welcome administrator %s. Glad to see you back! Ur activated too!" % data['username'], "success")
+            flash("Welcome administrator %s. Glad to see you back! Ur activated too!" % data['username'], "success")
         else:
             flash("Your account have been activated successfully!", "success")
     else:
