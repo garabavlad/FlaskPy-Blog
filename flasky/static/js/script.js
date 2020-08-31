1f2ca394b994bdb830a94a7759d373f66e3d6ce6
garaba.vlad <garaba.vlad@gmail.com>
2020-08-31 19:58:17 +0300

    Google reCAPTCHA added to login & register pages

--- flasky/static/js/script.js
+++ flasky/static/js/script.js
@@ -2,3 +2,12 @@ ClassicEditor
         .create(document.querySelector('#editor'))
         .catch(error => {}
     );
+
+
+// ReCAPTCHA form submits
+function onRegisterSubmit(token) {
+    document.getElementById("register-form").submit();
+}
+function onLoginSubmit(token) {
+    document.getElementById("login-form").submit();
+}
