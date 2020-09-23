d48d608b835a7ac31512cc0d9a7461974dfc05df
garaba.vlad <garaba.vlad@gmail.com>
2020-09-23 16:20:03 +0100

    User dashoard sidebar made responsive

--- flasky/static/js/udash.js
+++ flasky/static/js/udash.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
 
     $("#sidebar").mCustomScrollbar({
-         theme: "minimal"
+        theme: "minimal"
     });
 
 
@@ -14,6 +14,18 @@ $(document).ready(function () {
         // and also adjust aria-expanded attributes we use for the open/closed arrows
         // in our CSS
         $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    $('.udash-close-arrow').on('click', function () {
+        // open or close navbar
+        $('#sidebar').toggleClass('active');
+        $('.udash-content').toggleClass('fullscreen');
+        // close dropdowns
+        $('.collapse.in').toggleClass('in');
+        // and also adjust aria-expanded attributes we use for the open/closed arrows
+        // in our CSS
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+
     });
 
 });
